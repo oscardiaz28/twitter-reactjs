2f56270f17a5f7f64326221aa039c133aadbf2c9
oscardiaz28 <diazvargasod@gmail.com>
2024-12-05 08:32:01 -0500

    last changes

--- backend/server.js
+++ backend/server.js
@@ -44,9 +44,9 @@ app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
 
 if(true){
-    app.use(express.static(path.join(__dirname, "/frontend/build")))
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 }
 
